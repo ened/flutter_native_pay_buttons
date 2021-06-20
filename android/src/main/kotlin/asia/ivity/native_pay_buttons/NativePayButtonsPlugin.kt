30c5e6b16987cd96a9223bd17c57d2a158cd394d
Sebastian Roth <sebastian.roth@gmail.com>
2021-06-20 15:19:12 +0100

    modernize

--- android/src/main/kotlin/asia/ivity/native_pay_buttons/NativePayButtonsPlugin.kt
+++ android/src/main/kotlin/asia/ivity/native_pay_buttons/NativePayButtonsPlugin.kt
@@ -5,66 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
 /** NativePayButtonsPlugin */
-public class NativePayButtonsPlugin : FlutterPlugin, MethodCallHandler {
-    private var channel: MethodChannel? = null
-
+class NativePayButtonsPlugin : FlutterPlugin {
     override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(binding.binaryMessenger, "native_pay_buttons")
-        channel?.setMethodCallHandler(this)
-
-        binding.platformViewRegistry.registerViewFactory("asia.ivity/native_pay_button", object : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
-            override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
-                val layout = when (args?.let { it as Map<*, *> }?.let { it["style"] as? Int }
-                        ?: 0) {
-                    0 -> R.layout.googlepay_button
-                    1 -> R.layout.googlepay_button_no_shadow
-                    2 -> R.layout.buy_with_googlepay_button
-                    3 -> R.layout.buy_with_googlepay_button_no_shadow
-                    else -> R.layout.googlepay_button
-                }
-
-
-                val inflater = LayoutInflater.from(context)
-                return object : PlatformView {
-                    override fun getView(): View {
-                        return inflater.inflate(layout, null)
+        binding.platformViewRegistry.registerViewFactory(
+            "asia.ivity/native_pay_button",
+            object : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+                override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
+                    val layout = when (
+                        args?.let { it as Map<*, *> }?.let { it["style"] as? Int }
+                            ?: 0
+                    ) {
+                        0 -> R.layout.googlepay_button
+                        1 -> R.layout.googlepay_button_no_shadow
+                        2 -> R.layout.buy_with_googlepay_button
+                        3 -> R.layout.buy_with_googlepay_button_no_shadow
+                        else -> R.layout.googlepay_button
                     }
 
-                    override fun dispose() {
+                    val inflater = LayoutInflater.from(context)
+                    return object : PlatformView {
+                        override fun getView(): View {
+                            return inflater.inflate(layout, null)
+                        }
+
+                        override fun dispose() {
+                        }
                     }
                 }
             }
-        })
-    }
-
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "native_pay_buttons")
-            channel.setMethodCallHandler(NativePayButtonsPlugin())
-        }
-    }
-
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-        if (call.method == "getPlatformVersion") {
-            result.success("Android ${android.os.Build.VERSION.RELEASE}")
-        } else {
-            result.notImplemented()
-        }
+        )
     }
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel?.setMethodCallHandler(null)
-        channel = null
     }
 }
--- example/android/app/src/main/kotlin/asia/ivity/native_pay_buttons_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package asia.ivity.native_pay_buttons_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
